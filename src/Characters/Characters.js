4467f5868ac1b9dc29c111c886241e6b03c5803e
Mert keskin <mertkeskinmtk6@gmail.com>
2021-12-22 10:19:57 +0300

    created: projects files and folders

---
+++ src/Characters/Characters.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Characters = () => {
+  return (
+    <div>
+      this is the characters page
+    </div>
+  )
+}
+
+export default Characters;
---
+++ src/CharactersList/CharactersList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CharactersList = () => {
+  return (
+    <div>
+      this is the character list page
+    </div>
+  )
+}
+
+export default CharactersList;
